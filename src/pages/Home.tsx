e6d771b2a6809b1302b18b5fa747e3de463fac79
vicabats <vitoria_batistoti@hotmail.com>
2021-10-07 22:15:20 -0300

    Adding new functionalities

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,26 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(currentTasks => [...currentTasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const a = tasks.map(task => task.id === id ? {
+      ...task,
+      done: !task.done
+    } : task)
+
+    setTasks(a)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter((task: Task) => task.id !== id)
+    setTasks(updatedTasks)
   }
 
   return (
